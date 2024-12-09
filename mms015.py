d61d8640a2bc63b32a308e17a7367537a198b917
Espen Gabrielsen <espen.gabrielsen@optimera.no>
2024-12-09 09:29:21 +0100

    ferdig programmert func for å legge inn data i MMS015 mangler PSE

--- mms015.py
+++ mms015.py
@@ -14,63 +14,56 @@ def makeUnit(factor, unit, basicUnit, salesPriceUnit, procurmentUnit):
     else:
         AUS1 = 0
 
-    if unit == procurmentUnit:
+    if unit == procurmentUnit: # legger inn enhets flagg
         AUS2 = 1
     else:
         AUS2 = 0
 
-    if unit == salesPriceUnit:
+    if unit == salesPriceUnit: # legger inn pris flagg
         AUS5 = 1
         AUS9 = 1
         if factor != 1:
-            conversionForm = 2
+            factor = round(1/factor,3) # endrer faktor slik at salgsenhet kan deles (round slik at det ikke bli float feil)
+            conversionForm = 2 # settere converterings type flagg
     else:
         AUS5 = 0
         AUS9 = 0
-
+    
     for type in unitTypes:
         type = type.value
         if type == 1:
             print(
-                f"Type:{type} Factor:{factor} Unit:{unit} Aus1:{AUS1} Aus2:{AUS2} Conversion_Form:{conversionForm}"
+                f"Type:{type}\nFactor: {factor:5} Unit:{unit:3} Aus1:{AUS1} Aus2:{AUS2} Conversion_Form:{conversionForm}"
             )
         else:
             print(
-                f"Type:{type} Factor:{factor} Unit:{unit} Aus5:{AUS5} Aus9:{AUS9} Conversion_Form:{conversionForm}\n"
+                f"Type:{type}\nFactor: {factor:5} Unit:{unit:3} Aus5:{AUS5} Aus9:{AUS9} Conversion_Form:{conversionForm}\n"
             )
 
 
-factor: float = 1
-unit: str = "PAK"
 basicUnit: str = "PAK"
-salesPriceUnit: str = "STK"
+salesPriceUnit: str = "LM"
 procurmentUnit = "PAK"
 
-
-makeUnit(
-    factor=factor,
-    unit=unit,
-    basicUnit=basicUnit,
-    salesPriceUnit=salesPriceUnit,
-    procurmentUnit=procurmentUnit,
-)
-makeUnit(
-    factor=0.1,
-    unit="STK",
-    basicUnit=basicUnit,
-    salesPriceUnit=salesPriceUnit,
-    procurmentUnit=procurmentUnit,
-)
-makeUnit(
-    factor=10,
-    unit="PAL",
-    basicUnit=basicUnit,
-    salesPriceUnit=salesPriceUnit,
-    procurmentUnit=procurmentUnit,
-)
 makeUnit(
     factor=1,
-    unit="ESK",
+    unit="PAK",
+    basicUnit=basicUnit,
+    salesPriceUnit=salesPriceUnit,
+    procurmentUnit=procurmentUnit,
+)
+
+makeUnit(
+    factor=0.277777778,
+    unit="LM",
+    basicUnit=basicUnit,
+    salesPriceUnit=salesPriceUnit,
+    procurmentUnit=procurmentUnit,
+)
+
+makeUnit(
+    factor=165,
+    unit="PAL",
     basicUnit=basicUnit,
     salesPriceUnit=salesPriceUnit,
     procurmentUnit=procurmentUnit,
